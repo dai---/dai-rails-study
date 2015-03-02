07d444319bc2dd9a744c74f3c12c99817a7d0657
vagrant <vagrant@localhost.localdomain>
2015-03-02 18:32:06 +0000

    作成処理追加

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,28 @@
 class ProjectsController < ApplicationController
+
   def index
     @projects = Project.all
   end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      #paramsの中のprojectでわたってきたもののうち、タイトルだけをひっぱってくる。
+      #フィルタリングしてるのかなぁ
+      params[:project].permit(:title)
+    end
+
 end
